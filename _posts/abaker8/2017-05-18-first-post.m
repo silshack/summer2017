5c1dd56f8d35b887bb2cfee07cda6d6d57188568
abaker8 <abaker8@binghamton.edu>
2017-05-22 15:55:22 -0400

    Update 2017-05-18-first-post.m

--- _posts/abaker8/2017-05-18-first-post.m
+++ _posts/abaker8/2017-05-18-first-post.m
@@ -3,5 +3,5 @@ layout: post
 author: abaker8
 title: "Amanda's first post"
 ---
-I am a masters of information science student who loves cooking, sports and animals.      
+    I am a masters of information science student who loves cooking, sports and animals.      
     
